2ceeed9b05f8faa4f2ff50e579f03ab8c3b923b0
BrenaElle <brena.ellen@academico.ifpb.edu.br>
2019-06-13 17:24:14 -0300

    Atualizado

---
+++ script4.js
@@ -0,0 +1,40 @@
+
+// Apertou o Mouse
+ function ApertouOmouse(){
+       console.log("O mouse foi apertado!");
+}
+
+//Mouse Up
+function SolteiOmouse(){
+      console.log("Soltei o botao do Mouse!");
+}
+
+//Mouse Over
+function PassouOmouse(){
+    console.log("voce passou o mouse em cima do botao! ");
+}
+
+//Mouse Out
+function TirouOmouse(){
+    console.log("voce esta mexendo o mouse pelo botao!");
+}
+
+//Mouse Move
+function MovendoOmouse(){
+    console.log("voce esta mexendo sobre o botao");
+}
+
+//On Click
+ function onclick(){
+     console.log("Voce clicou no botao");
+ }
+
+ //Context Menu
+ function BotaoDireito(){
+     console.log("voce clicou com o botao direito do mouse!");
+
+     return false;
+ }
+
+
+     
