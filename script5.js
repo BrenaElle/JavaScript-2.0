5d377d2f79eb8278634ca64cd21a940107ccff95
BrenaElle <brena.ellen@academico.ifpb.edu.br>
2019-06-13 18:01:10 -0300

    Aprendendo a usar os Eventos do Teclado

---
+++ script5.js
@@ -0,0 +1,18 @@
+
+// Apertou o Teclado
+function TecladoApertado(event){
+    console.log("Teclado apertado!"+ event.ctrlkey);
+}
+
+
+//Soltou o Teclado
+function SoltouOteclado(event){
+   console.log("Soltou o Teclado!");
+}
+
+//Teclado Apertado
+function ApertounoTeclado(event){
+ console.log("Apertou uma Tecla! ");
+}
+
+
