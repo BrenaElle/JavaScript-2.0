b382a9ace5c960ec25279e235728d432a109828e
BrenaElle <brena.ellen@academico.ifpb.edu.br>
2019-06-17 15:22:14 -0300

    Aprendendo Eventos de Funcionários

---
+++ script6.js
@@ -0,0 +1,16 @@
+
+function focado(){
+  console.log("Campo Focado!");
+} 
+
+function desfocado(){
+   console.log("Desfocou do Campo!");
+}
+
+function OpçãoSelecionada(objeto){
+    console.log("Cidade selecionada:" + objeto.value);
+}
+
+function hgfjh(){
+      console.log("");
+}
